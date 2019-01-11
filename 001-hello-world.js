6cdd8ebd29b79851e9b951b6607ac7a3c8b07c0c
Shaun Johansen <shaunjohansen@gmail.com>
2019-01-11 15:10:23 -0400

    add hello-world server

---
+++ 001-hello-world.js
@@ -0,0 +1,21 @@
+const http = require('http')
+const port = Number(process.env.PORT) || 8000
+
+const requestHandler = (request, response) => {
+  console.log(request.method, request.url)
+  console.log('Request Headers:', request.headers)
+
+  // response.end('Greetings from your friendly neighourhood nodejs server!')
+  response.setHeader('Content-Type', 'application/json')
+  response.end(JSON.stringify(request.headers, null, 2))
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (error) => {
+  if (error) {
+    return console.error('error starting server:', error)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
